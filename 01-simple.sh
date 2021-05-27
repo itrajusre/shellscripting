e204425cf195a49c2b9ce9c95cd36a3b5d84e3f2
itrajusre <iraju2807@gmail.com>
2021-05-27 16:32:04 +0530

    Addition of two values2

--- 01-simple.sh
+++ 01-simple.sh
@@ -2,8 +2,9 @@
 #cat /etc/passwd
 #whoami
 #uptime
-echo "******* WELCOME TO SHELL SCRIPTING *******"
+echo " ******* WELCOME TO SHELL SCRIPTING ******* "
 a=5
 b=10
 c= ` expr $a + $b `
 echo "Addition value "$c
+echo "End of the SCRIPTING"
