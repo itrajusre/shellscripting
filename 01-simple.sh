a4f60f4ab400ef350bd1443f55d572a68df208f5
itrajusre <iraju2807@gmail.com>
2021-05-28 06:34:51 +0530

    added 2 lines extra

--- 01-simple.sh
+++ 01-simple.sh
@@ -9,5 +9,7 @@ c=`expr $b - $a`
 echo "               Subtractinon value: $c            "
 echo -e "\e[31mHello in Red color\e[0m"
 echo -e "\e[32mHello in Green color\e[0m"
+echo -e "\e[33mHello in Green color\e[0m"
+echo -e "\e[34mHello in Green color\e[0m"
 
 
