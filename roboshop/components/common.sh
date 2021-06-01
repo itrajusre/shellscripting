9ee6c2245b235bf8a3d610472a1dcbc603b3d628
itrajusre <iraju2807@gmail.com>
2021-06-01 12:16:07 +0530

    Edited common.sh file

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -7,7 +7,7 @@ HEAD()
 STAT()
 {
   if [ $1 -eq 0 ]; then
-    echo -n -e "\e[1;32m done\e[0m"
+    echo -e "\e[1;32m done\e[0m \t"
     #echo -e "[\e[1;32mSUCC\e[0m] [\e[1;35m${COMPONENT}\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] $1"
   else
     echo -e "\e[1;31m fail\e[0m"
